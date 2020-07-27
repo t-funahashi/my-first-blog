73cb14c71a4546d6e4406b3c09ae1c1b8599f766
t-funahashi <t-funahashi@imagenics.co.jp>
2020-07-27 16:44:45 +0900

    Changed sisettings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!%_yz4uf(kn97h!!e194%zigt0xqd)lw+u93gxu!0d!%)tk-$k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
